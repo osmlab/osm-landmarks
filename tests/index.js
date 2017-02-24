3db7da49b4e88d815a234b893726b1b1df287c6c
Bhargav Kowshik <bhargav@mapbox.com>
2017-02-24 15:44:38 +0530

    Add a couple of lakes to landmarks

---
+++ tests/index.js
@@ -0,0 +1,23 @@
+var test = require('tape');
+var csv = require('csv');
+var path = require('path');
+var fs = require('fs');
+
+test('Test format of csv files', function(t) {
+    var dirname = path.join(__dirname, '../landmarks/');
+    var files = fs.readdirSync(dirname);
+
+    // Filter out only csv files in the directory.
+    files = files.filter(function(filename) { return /.csv$/.test(filename); });
+
+    files.forEach(function(filename) {
+        csv.parse(fs.readFileSync(path.join(dirname, filename)), function (error, rows) {
+            if (error) t.fail(error);
+
+            header = rows[0];
+            t.true(header.indexOf('id') !== -1, filename + ' has header "id"');
+            t.true(header.indexOf('type') !== -1, filename + ' has header "type"');
+        });
+    });
+    t.end();
+});
