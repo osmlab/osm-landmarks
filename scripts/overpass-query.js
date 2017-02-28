71a38d8393f30f6a08b8fbf3db6a87e29dec964a
amishas157 <amishas157@gmail.com>
2017-02-28 23:20:54 +0530

    Use geographical and levenshtein distance for matching

--- scripts/overpass-query.js
+++ scripts/overpass-query.js
@@ -4,6 +4,7 @@ var util = require('util');
 var fs = require('fs');
 var csv = require('csv');
 var queue = require('d3-queue').queue;
+var Levenshtein = require('levenshtein');
 
 if (!argv.wikidata) {
     console.log('');
@@ -15,36 +16,57 @@ if (!argv.wikidata) {
     return;
 }
 
-function queryOverpass(wikidataID, callback) {
+function queryOverpass(wikidataID, restaurantName, lat, lon, callback) {
     // console.log(wikidataID);
     // wikidataID = 'Q5359767';
     var query = fs.readFileSync(__dirname + '/overpass-query.ql').toString();
-    query = query.replace(/wikidataID/g, wikidataID);
+    query = query.replace(/lat/g, lat);
+    query = query.replace(/lon/g, lon);
 
     overpass(query, function(error, data) {
         if (error) return callback(error, null);
 
         var osmID = '';
-        if (data.features.length > 0) {
-            osmID = data.features[0]["id"];
+        var osmName = '';
+        var osmType = '';
+        var minDistance = 100;
+        for (var i = 0; i < data.features.length; i++) {
+            try {
+                var distance = new Levenshtein(data.features[i].properties.tags.name, restaurantName).distance;
+                if (distance < minDistance) {
+                    minDistance = distance;
+                    osmID = data.features[i].properties.id;
+                    osmName = data.features[i].properties.tags.name;
+                    osmType = data.features[i].properties.type;
+                }
+            }
+            catch (e) {
+                //do nothing
+            }
         }
-        console.log(wikidataID + ',' + osmID);
-        callback(error, osmID)
+        console.log(wikidataID + ',' + restaurantName + ',' + osmID + ',' + osmType + ',' + osmName + ',' + minDistance);
+        callback(error, osmID);
     });
 }
 
 var q = queue(1);
 
 csv.parse(fs.readFileSync(argv.wikidata), function (error, rows) {
-    header = []
+    header = [];
     for (var i = 0; i < rows.length; i++) {
         if (header.length === 0) {
             header = rows[i];
             continue;
         }
         var wikidataID = rows[i][0].split('/').slice(-1)[0];
-        // console.log(wikidataID);
-        q.defer(queryOverpass, wikidataID);
+        var latlon = rows[i][3];
+        var coordinates = '';
+        if (latlon.length > 0) {
+            coordinates = latlon;
+            var lon = (coordinates.split('(')[1]).split(' ')[0];
+            var lat = coordinates.split('(')[1].split(' ')[1].split(')')[0];
+            q.defer(queryOverpass, wikidataID, rows[i][1], lat, lon);
+        }
     }
 
     q.awaitAll(function(error, results) {
